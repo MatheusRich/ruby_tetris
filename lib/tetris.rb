632b56fc7c084b711b4a76a4c3d009a3340b1d12
Matheus Richard <matheusrichardt@gmail.com>
2020-08-24 23:47:27 -0300

    Render piece on screen

--- lib/tetris.rb
+++ lib/tetris.rb
@@ -3,18 +3,21 @@
 require_relative './fake_io'
 require_relative './field'
 
+TETROMINO_WIDTH = 4
+TETROMINO_HEIGHT = 4
+
 def build_assets
-  tetromino = Array.new(7, '')
+  tetrominos = Array.new(7, '')
 
-  tetromino[0] += '..X...X...X...X.'
-  tetromino[1] += '..X..XX..X......'
-  tetromino[2] += '.X...XX...X.....'
-  tetromino[3] += '.....XX..XX.....'
-  tetromino[4] += '..X..XX...X.....'
-  tetromino[5] += '.....XX...X...X.'
-  tetromino[6] += '.....XX..X...X..'
+  tetrominos[0] += '..X...X...X...X.'
+  tetrominos[1] += '..X..XX..X......'
+  tetrominos[2] += '.X...XX...X.....'
+  tetrominos[3] += '.....XX..XX.....'
+  tetrominos[4] += '..X..XX...X.....'
+  tetrominos[5] += '.....XX...X...X.'
+  tetrominos[6] += '.....XX..X...X..'
 
-  tetromino
+  tetrominos
 end
 
 DEG_0 = 0
@@ -25,7 +28,7 @@ DEG_270 = 3
 def rotate(x, y, degree)
   case degree % 4
   when DEG_0
-    (y * 4) - x
+    (y * 4) + x
   when DEG_90
     12 + y - (x * 4)
   when DEG_180
@@ -37,14 +40,38 @@ def rotate(x, y, degree)
   end
 end
 
+def piece_fit?(tetromino, rotation, pos_x, pos_y)
+  (0...TETROMINO_WIDTH).each do |x|
+    (0...TETROMINO_HEIGHT).each do |y|
+      piece_index = rotate(pos_x, pos_y, rotation)
+
+      tile_x = (pos_x + x)
+      tile_y = (pos_y + y)
+
+      next unless tile_x >= 0 && tile_x < Field::WIDTH
+
+      if tile_y >= 0 && tile_y < Field::HEIGHT
+        return false if $tetrominos[tetromino][piece_index] == 'X' && $field.empty_at?(tile_x, tile_y)
+      end
+    end
+  end
+
+  true
+end
+
+LETTER_A_IN_ASCII = 65
+def piece_tile_for(piece_index)
+  (piece_index + LETTER_A_IN_ASCII).chr
+end
+
 ########################### MAIN ###########################
 
-SCREEN_WIDTH = 30
+SCREEN_WIDTH = 22
 SCREEN_HEIGHT = 16
 DRAW_OFFSET = 2
 io = FakeIO.new(SCREEN_WIDTH, SCREEN_HEIGHT)
-tetrominos = build_assets
-field = Field.new
+$tetrominos = build_assets
+$field = Field.new
 screen = []
 
 (0...SCREEN_WIDTH).each do |x|
@@ -53,13 +80,43 @@ screen = []
   end
 end
 
+# Game logic stuff
+
 game_over = false
+current_piece = 0
+current_rotation = DEG_0
+current_x = Field::WIDTH / 2
+current_y = 0
 
 until game_over
-  field.each_coord do |x, y|
-    screen[(y + DRAW_OFFSET) * SCREEN_WIDTH + (x + DRAW_OFFSET)] = ' ABCDEFG=#'[field.at(x, y)]
+  # Game timing =======================
+
+  # Input =============================
+
+  # Game logic ========================
+
+  # Render output =======================
+
+  # Draw field
+  $field.each_coord do |x, y|
+    tile = (y + DRAW_OFFSET) * SCREEN_WIDTH + (x + DRAW_OFFSET)
+    screen[tile] = ' ABCDEFG=#'[$field.at(x, y)]
   end
 
+  # Draw current piece
+  (0...TETROMINO_WIDTH).each do |x|
+    (0...TETROMINO_HEIGHT).each do |y|
+      if $tetrominos[current_piece][rotate(x, y, current_rotation)] == 'X'
+        screen[
+          (current_y + y + DRAW_OFFSET) * SCREEN_WIDTH + (current_x + x + DRAW_OFFSET)
+        ] = piece_tile_for(current_piece)
+      end
+    end
+  end
+
+  # Display frame
   io.write(screen)
-  break
+
+  sleep 2
+  current_piece = (current_piece + 1) % 7
 end
